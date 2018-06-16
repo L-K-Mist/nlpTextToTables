3fa940b16c91e250a0c92bb8557e5aac655c33ff
Dylan <dylan@vandenbosch.co.za>
2018-06-16 14:03:22 +0200

    quicksave on branch to pull in db features from master

--- src/components/DailyLog/LogOutput.vue
+++ src/components/DailyLog/LogOutput.vue
@@ -1,13 +1,13 @@
 <template>
     <v-card>
         <v-list two-line>
-            <template v-for="(item, index) in finSentences">
+            <template v-for="(item, index) in finSentences.verified">
                 <v-list-tile :key="index" avatar>
                     <v-list-tile-avatar>
                         <emoji  emoji="money_with_wings" :size="40"></emoji>
                     </v-list-tile-avatar>
                     <v-list-tile-content>
-                        <v-list-tile-title v-html="item.text"></v-list-tile-title>
+                        <v-list-tile-title v-html="item"></v-list-tile-title>
                     </v-list-tile-content>
                 </v-list-tile>
             </template>
@@ -21,7 +21,8 @@ export default {
       return this.$store.getters.gotFin;
     },
     finSentences() {
-      return this.$store.getters.finSentences;
+      console.log("LogOutput has", this.$store.getters.suppliersEvaluated);
+      return this.$store.getters.suppliersEvaluated;
     }
   }
 };
